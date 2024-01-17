52fb3e8a3096dd14657ecf3f81b9de7fbbbfbe5f
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-17 13:48:29 +0800

    added score counter

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 function getComputerChoice(){
     let choices = ["rock","paper","scissors"];
     let random = Math.floor(Math.random()*3);
@@ -19,7 +21,7 @@ function playRound(playerSelection,computerSelection){
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")){
 
-        resultText = `You Win! '${playerSelection.toUpperCase()}' beats '${computerSelection.toUpperCase()}'` ;
+        resultText = `You win! '${playerSelection.toUpperCase()}' beats '${computerSelection.toUpperCase()}'` ;
         return resultText;
     }
     if((computerSelection === "rock" && playerSelection === "scissors") ||
@@ -30,11 +32,19 @@ function playRound(playerSelection,computerSelection){
         return resultText;
     }
 }
+let getPlayerChoice = "rock"
 
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let getPlayerChoice = prompt("Rock, paper, scissors? ");
-    let roundWinner = playRound(getPlayerChoice,getComputerChoice())
-    return roundWinner;
+    let roundResult = playRound(getPlayerChoice,getComputerChoice());
+    console.log(roundResult)
+    let lookForWin = "You win!";
+    let lookForLose = "You lose";
+    if(roundResult.includes(lookForWin)){
+        playerScore++;
+    }
+    else if( roundResult.includes(lookForLose)){
+        computerScore++
+    }
+    console.log(playerScore);
+    console.log(computerScore);
 }
